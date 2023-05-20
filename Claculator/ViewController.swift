42703c01dc0aafe7a7cd13b14428356d85e43a1d
Eduard <eduardkarimov.rb@gmail.com>
2023-05-20 21:03:55 +0500

    Initial Commit

---
+++ Claculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Claculator
+//
+//  Created by Eduard Karimov on 20.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
